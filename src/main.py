8d92a49fc4168ef3de7ae84e3f8a0c32cd2de882
Willy <willy_1020@naver.com>
2024-10-24 02:27:07 +0900

    [✨feat/#8] 데이터셋을 폴더형태로 구분하여 저장

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import logging
 from datetime import datetime
 from dataset_loader import load_wmt_dataset, load_squad_dataset, load_cnn_daily_dataset
-from dataset_to_txt import save_and_compress_dataset_to_zip
+from dataset_to_txt import save_dataset_to_folder
 from evaluate_metrics import calculate_bleu, calculate_rouge, calculate_bert_score, calculate_combined_metric
 from visualize import visualize_scores, visualize_ngram_weight_experiment_results, visualize_weight_experiment_results
 from wmt_trans import translate_wmt_english_to_german
@@ -66,27 +66,27 @@ def run_weight_experiments_with_visualization(dataset_name, references, hypothes
     # 실험 결과 시각화
     visualize_weight_experiment_results(dataset_name, pos_weights_list, combined_scores)
 
-start_time = datetime.now()
-logging.info("프로그램 실행 시작")
+# start_time = datetime.now()
+# logging.info("프로그램 실행 시작")
 
 # WMT 데이터셋 불러오기
 wmt_dataset = load_wmt_dataset()
 wmt_english_data = [item['en'] for item in wmt_dataset]
 wmt_references = translate_wmt_english_to_german(wmt_english_data) # 영어 문장을 독일어로 번역
 wmt_hypotheses = [item['de'] for item in wmt_dataset] # 독일어 문장
-# save_and_compress_dataset_to_zip((wmt_references, wmt_hypotheses), "wmt")  # WMT 데이터셋 텍스트 파일로 저장
+save_dataset_to_folder((wmt_references, wmt_hypotheses), "WMT")
 
 # SQuAD 데이터셋 불러오기
 squad_contexts, squad_questions, squad_answers = load_squad_dataset()
 squad_references = [answer['text'][0] for answer in squad_answers]  # 정답 텍스트 리스트 추출
 squad_hypotheses = squad_contexts  # 문맥이 번역본이라고 가정
-# save_and_compress_dataset_to_zip((squad_contexts, squad_questions, squad_answers), "squad")  # SQuAD 데이터셋 텍스트 파일로 저장
+save_dataset_to_folder((squad_contexts, squad_references, squad_hypotheses), "SQuAD")
 
 # CNN/DAILYMAIL 데이터셋 불러오기
 cnn_articles, cnn_highlights = load_cnn_daily_dataset()
 cnn_references = cnn_highlights  # 요약문이 정답이라고 가정
 cnn_hypotheses = cnn_articles  # 기사 본문이 번역본이라고 가정
-# save_and_compress_dataset_to_zip((cnn_articles, cnn_highlights), "cnn")  # CNN/DailyMail 데이터셋 텍스트 파일로 저장
+save_dataset_to_folder((cnn_articles, cnn_references), "CNN_DailyMail")
 
 # 데이터 양을 줄이기 위해 샘플링 (예: 처음 100개만 사용)
 num_samples = 100
@@ -148,9 +148,9 @@ combined_scores = [wmt_combined, squad_combined, cnn_combined]
 # 점수 시각화
 visualize_scores(bleu_scores, rouge_scores, bert_scores, combined_scores)
 
-end_time = datetime.now()
-logging.info("프로그램 종료")
-
-# 실행 시간 계산 및 출력
-elapsed_time = end_time - start_time
-logging.info(f"프로그램 총 실행 시간: {elapsed_time}")
+# end_time = datetime.now()
+# logging.info("프로그램 종료")
+#
+# # 실행 시간 계산 및 출력
+# elapsed_time = end_time - start_time
+# logging.info(f"프로그램 총 실행 시간: {elapsed_time}")
