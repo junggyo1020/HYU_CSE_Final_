a2a7b78a4a627b8a8bcbfc970b84861440b7cf34
Willy <willy_1020@naver.com>
2024-11-14 19:36:13 +0900

    [✨feat/#9] 새로운 평가 데이터셋(STS) 적용 및 기존 평가 지표의 파라미터 개선 - 새로운 평가 데이터셋(STS)을 도입했습니다. - SQuAD의 기존의 파라미터를 기존 데이터의 answer와 QA라이브러리를 통해 얻어낸 answer 데이터로 사용하도록 변경했습니다.

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -1,6 +1,10 @@
 # dataset_loader.py
 
 from datasets import load_dataset
+from transformers import pipeline
+
+# QA 모델 로드
+qa_model = pipeline("question-answering")
 
 def load_wmt_dataset():
     print("Loading WMT dataset...")
@@ -12,13 +16,30 @@ def load_squad_dataset():
     print("Loading SQuAD dataset...")
     dataset = load_dataset("squad", split="validation[:100]")
     print("SQuAD dataset loaded.")
-    return dataset['context'], dataset['question'], dataset['answers']
+
+    contexts = dataset['context']
+    questions = dataset['question']
+    original_answers = dataset['answers']
+
+    generated_answers = []
+    for context, question in zip(contexts, questions):
+        result = qa_model(question=question, context=context)
+        generated_answers.append(result['answer'])
+
+    print("SQuAD dataset proceed with QA model.")
+    return original_answers, generated_answers
 
 def load_cnn_daily_dataset():
     print("Loading CNN/DailyMail dataset...")
     dataset = load_dataset("cnn_dailymail", "3.0.0", split="test[:100]")
     print("CNN/DailyMail dataset loaded.")
     return dataset['article'], dataset['highlights']
+
+def load_sts_dataset():
+    print("Loading STS dataset...")
+    dataset = load_dataset("stsb_multi_mt", name="en", split="test[:100]")
+    print("STS dataset loaded.")
+    return dataset['sentence1'], dataset['sentence2']
 
 # # 여러 개의 질문-지문 쌍을 일괄 처리
 # data = [
